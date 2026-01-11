913509f0a776b7214a8e00dfbd741e7cace63e7d
rebelgoodies <rebelgoodies@users.noreply.github.com>
2026-01-11 00:38:08 -0700

    vendor: update to FotR 1.4 upstream files

--- mod/Data/Scripts/Library/eawx-plugins/hero-respawn/HeroRespawn.lua
+++ mod/Data/Scripts/Library/eawx-plugins/hero-respawn/HeroRespawn.lua
@@ -23,6 +23,7 @@ require("PGSpawnUnits")
 require("deepcore/std/class")
 require("eawx-util/StoryUtil")
 require("PGDebug")
+require("SetFighterResearch")
 
 HeroRespawn = class()
 
@@ -30,6 +31,7 @@ function HeroRespawn:new(herokilled_finished_event, human_player)
     self.human_player = human_player
     herokilled_finished_event:attach_listener(self.on_galactic_hero_killed, self)
 	self.durge_chance = 105
+	self.dooku_died = false
 end
 
 function HeroRespawn:on_galactic_hero_killed(hero_name, owner)
@@ -37,16 +39,30 @@ function HeroRespawn:on_galactic_hero_killed(hero_name, owner)
 	
 	if hero_name == "GRIEVOUS_TEAM_RECUSANT" then
 		self:spawn_grievous("Grievous_Team_Munificent","GRIEVOUS_RESPAWN_MUNIFICENT")
+		Transfer_Fighter_Hero("GRIEVOUS_RECUSANT", "GRIEVOUS_MUNIFICENT")
     elseif hero_name == "GRIEVOUS_TEAM" then
 		self:spawn_grievous("Grievous_Team_Munificent","GRIEVOUS_RESPAWN_MUNIFICENT")
+		Transfer_Fighter_Hero("INVISIBLE_HAND", "GRIEVOUS_MUNIFICENT")
 	elseif hero_name == "GRIEVOUS_TEAM_MALEVOLENCE" then
 		self:spawn_grievous("Grievous_Team","GRIEVOUS_RESPAWN_INVISIBLE_HAND")
+		Transfer_Fighter_Hero("GRIEVOUS_MALEVOLENCE", "INVISIBLE_HAND")
 	elseif hero_name == "GRIEVOUS_TEAM_MALEVOLENCE_2" then
 		self:spawn_grievous("Grievous_Team_Recusant","GRIEVOUS_RESPAWN_RECUSANT")
+		Transfer_Fighter_Hero("GRIEVOUS_MALEVOLENCE_2", "GRIEVOUS_RECUSANT")
+	elseif hero_name == "GRIEVOUS_MUNIFICENT_GROUND" then
+		GlobalValue.Set("GRIEVOUS_DEAD", true)
 	elseif hero_name == "DURGE_TEAM" then
 		self:check_durge()
+	elseif hero_name == "DOOKU_TEAM" then
+		self:check_dooku_doppelganger()
+	elseif hero_name == "YULAREN_RESOLUTE" then
+		self:spawn_yularen("Yularen_Integrity")
+	elseif hero_name == "YULAREN_INVINCIBLE" then
+		self:spawn_yularen("Yularen_Integrity")
 	elseif hero_name == "TRENCH_INVINCIBLE" then
 		self:start_cyber_trench_countdown()
+	elseif hero_name == "ZOZRIDOR_SLAYKE_CARRACK" then
+		self:slaykes_second_chance()
 	end
 end
 
@@ -71,6 +87,29 @@ function HeroRespawn:spawn_grievous(team, event)
 	end
 end
 
+function HeroRespawn:spawn_yularen(team)
+	--Logger:trace("entering HeroRespawn:spawn_yularen")
+
+	local p_republic = Find_Player("Empire")
+	local planet
+	local capital = Find_First_Object("Remnant_Capital")
+	if TestValid(capital) then
+		planet = capital.Get_Planet_Location()
+	end
+	if not TestValid(planet) then
+		planet = StoryUtil.FindFriendlyPlanet(p_republic)
+	end
+	if not StoryUtil.CheckFriendlyPlanet(planet,p_republic) then
+		planet = StoryUtil.FindFriendlyPlanet(p_republic)
+	end
+	if planet then
+		SpawnList({team}, planet, p_republic, true, false)
+		if Find_Player("Empire").Is_Human() then
+			StoryUtil.Multimedia("TEXT_SPEECH_YULAREN_RETURNS_INTEGRITY", 15, nil, "Piett_Loop", 0)
+		end
+	end
+end
+
 function HeroRespawn:check_durge()
 	--Logger:trace("entering HeroRespawn:check_durge")
 
@@ -80,15 +119,33 @@ function HeroRespawn:check_durge()
 		local planet = StoryUtil.FindFriendlyPlanet(p_CIS)
 		if planet then
 			SpawnList({"Durge_Team"}, planet, p_CIS, true, false)
-			Story_Event("DURGE_RESPAWNS")
+			StoryUtil.Multimedia("TEXT_SPEECH_DURGE_RETURNS", 20, nil, "Durge_Loop", 0)
 			self.durge_chance = self.durge_chance - 10
 			StoryUtil.ShowScreenText("Revive chance: " .. tostring(self.durge_chance), 5)
 		else
-			Story_Event("DURGE_GONE")
+			StoryUtil.Multimedia("TEXT_SPEECH_DURGE_GONE", 20, nil, "Durge_Loop", 0)
 		end
 	else
-		Story_Event("DURGE_GONE")
+		StoryUtil.Multimedia("TEXT_SPEECH_DURGE_GONE", 20, nil, "Durge_Loop", 0)
 	end
+end
+
+function HeroRespawn:check_dooku_doppelganger()
+	--Logger:trace("entering HeroRespawn:check_dooku_doppelganger")
+	if self.dooku_died == false then
+		local p_CIS = Find_Player("Rebel")
+		local planet = StoryUtil.FindFriendlyPlanet(p_CIS)
+		StoryUtil.SpawnAtSafePlanet("SERENNO", p_CIS, StoryUtil.GetSafePlanetTable(), {"Dooku_Team"})
+		StoryUtil.Multimedia("TEXT_SPEECH_DOOKU_DOPPELGANGER_SPAWN", 15, nil, "Dooku_Loop", 0)
+		self.dooku_died = true
+	end
+end
+
+function HeroRespawn:slaykes_second_chance()
+	--Logger:trace("entering HeroRespawn:slaykes_second_chance")
+	local p_republic = Find_Player("Empire")
+	local planet = StoryUtil.FindFriendlyPlanet(p_republic)
+	StoryUtil.SpawnAtSafePlanet("ERIADU", p_republic, StoryUtil.GetSafePlanetTable(), {"Zozridor_Slayke_CR90"})
 end
 
 function HeroRespawn:start_cyber_trench_countdown()
